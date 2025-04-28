f8df4920226980aaa846a52c9a271ad1bf17c7f4
David Pereira <inexhero@gmail.com>
2025-04-28 10:48:10 -0500

    now use ram on dev mode

--- app.js
+++ app.js
@@ -2,42 +2,27 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { html } from 'htm/react';
 import Layout from './components/layout'
-import { createTheme, MantineProvider, Container, Notification } from '@mantine/core';
+import { MantineProvider, Container, Notification } from '@mantine/core';
 
 import { NotificationProvider} from './lib/runtime/notification'
 
 import {MercuryProvider } from './lib/runtime'
 import { ContextMenuProvider } from 'mantine-contextmenu';
 
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
-
 const root = createRoot(document.querySelector('#root'))
-
-const darkTheme = createTheme({
-  palette:{
-
-    mode:'dark'
-  }
-})
 
 function App(){
 
   return (
     html`
         <${MantineProvider} defaultColorScheme="dark">
-
-              <${MantineProvider} defaultColorScheme="auto">
-                  <${NotificationProvider}>
-                  <${ContextMenuProvider}>
-                  <${MercuryProvider}>
-                    <${Layout}/>
-                  <//>
-                  <//>
-                  <//>
+          <${NotificationProvider}>
+            <${ContextMenuProvider}>
+              <${MercuryProvider}>
+                <${Layout}/>
               <//>
-
+             <//>
+          <//>
         </${MantineProvider}>
 
     `
