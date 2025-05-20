c23e9ed23da36bdf9eea757e304e7cb2220f4b6b
nexhero <inexhero@gmail.com>
2025-05-20 17:11:41 -0500

    fix bug

--- components/renderAutocompeteOption.js
+++ components/renderAutocompeteOption.js
@@ -3,10 +3,11 @@ import { html } from "htm/react";
 import { Group, Text, Box } from "@mantine/core";
 export default function renderAutocompleteOpt({setTag,option,...props}){
     const handleClick = ()=>{
+        console.log(option)
         setTag(option.value)
     }
     return html`
-<${Group} gap="sm" onClick=${handleClick}>
+<${Group} gap="sm" onClick=${handleClick} style=${{ width:'100%'}}>
 <${Text} >${option.icon} ${option.label}</${Text}>
 
 </${Group}>
